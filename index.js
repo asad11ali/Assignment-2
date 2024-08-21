4d2ec1e3aaf1bcc015e53e83a23307c90876d4a8
asad11ali <info.asad11ali@gmail.com>
2024-08-21 20:49:45 +0500

    upload full setup

--- index.js
+++ index.js
@@ -137,3 +137,144 @@ function hidePopup() {
 }
 showPopup();
 hidePopup();
+
+let cardData = [
+  {
+    title : "Profesional Art <br>Printing Data",
+    discription : "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    item1 : "HTML",
+    item2 : "Bootstrap",
+    item3 : "Ruby",
+    btn : "See project",
+  },  {
+    title : "Profesional Art <br>Printing Data",
+    discription : "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    item1 : "HTML",
+    item2 : "Bootstrap",
+    item3 : "Ruby",
+    btn : "See project",
+  },  {
+    title : "Profesional Art <br>Printing Data",
+    discription : "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    item1 : "HTML",
+    item2 : "Bootstrap",
+    item3 : "Ruby",
+    btn : "See project",
+  },  {
+    title : "Profesional Art <br>Printing Data",
+    discription : "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    item1 : "HTML",
+    item2 : "Bootstrap",
+    item3 : "Ruby",
+    btn : "See project",
+  },  {
+    title : "Profesional Art <br>Printing Data",
+    discription : "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    item1 : "HTML",
+    item2 : "Bootstrap",
+    item3 : "Ruby",
+    btn : "See project",
+  },  {
+    title : "Profesional Art <br>Printing Data",
+    discription : "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    item1 : "HTML",
+    item2 : "Bootstrap",
+    item3 : "Ruby",
+    btn : "See project",
+  },
+  {
+    title : "Profesional Art <br>Printing Data",
+    discription : "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    item1 : "HTML",
+    item2 : "Bootstrap",
+    item3 : "Ruby",
+    btn : "See project",
+  }
+];
+
+
+
+  const cardContainer = document.querySelector(".card-tablate");
+ 
+      // Create card element using template literals
+      cardContainer.innerHTML = `
+      <div class="card-tablate-1">
+      <div class="block2" >
+            <h2>${cardData[0].title}</h2>
+                <p>
+                   ${cardData[0].discription}
+                </p>
+                <span class="card-item">
+                    <div>${cardData[0].item1}</div>
+                    <div>${cardData[0].item2}</div>
+                    <div>${cardData[0].item3}</div>
+                </span>
+                <button class="btn-2">${cardData[0].btn}</button>
+        </div>
+          <div class="block2" >
+                <h2>${cardData[1].title}</h2>
+                <p>
+                   ${cardData[1].discription}
+                </p>
+                <span class="card-item">
+                    <div>${cardData[1].item1}</div>
+                    <div>${cardData[1].item2}</div>
+                    <div>${cardData[1].item3}</div>
+                </span>
+                <button class="btn-2">${cardData[1].btn}</button>
+        </div>
+        </div>
+        <div class="card-tablate-2">
+          <div class="block2" >
+                <h2>${cardData[2].title}</h2>
+                <p>
+                   ${cardData[2].discription}
+                </p>
+                <span class="card-item">
+                    <div>${cardData[2].item1}</div>
+                    <div>${cardData[2].item2}</div>
+                    <div>${cardData[2].item3}</div>
+                </span>
+                <button class="btn-2">${cardData[2].btn}</button>
+        </div>
+          <div class="block2" >
+                <h2>${cardData[3].title}</h2>
+                <p>
+                   ${cardData[3].discription}
+                </p>
+                <span class="card-item">
+                    <div>${cardData[3].item1}</div>
+                    <div>${cardData[3].item2}</div>
+                    <div>${cardData[3].item3}</div>
+                </span>
+                <button class="btn-2">${cardData[3].btn}</button>
+        </div>
+        </div>
+        <div class="card-tablate-3">
+          <div class="block2" >
+                <h2>${cardData[4].title}</h2>
+                <p>
+                   ${cardData[4].discription}
+                </p>
+                <span class="card-item">
+                    <div>${cardData[4].item1}</div>
+                    <div>${cardData[4].item2}</div>
+                    <div>${cardData[4].item3}</div>
+                </span>
+                <button class="btn-2">${cardData[4].btn}</button>
+        </div>
+         <div class="block2" >
+                <h2>${cardData[5].title}</h2>
+                <p>
+                   ${cardData[5].discription}
+                </p>
+                <span class="card-item">
+                    <div>${cardData[5].item1}</div>
+                    <div>${cardData[5].item2}</div>
+                    <div>${cardData[5].item3}</div>
+                </span>
+                <button class="btn-2">${cardData[5].btn}</button>
+        </div>
+        </div>
+         
+      `;
